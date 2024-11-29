c38e9fca19d1d61338ba7a37d61e50483d6e75f2
Liam Townsley <contact@liamtownsley.me>
2024-11-29 16:23:46 +0000

    fix

--- Database.py
+++ Database.py
@@ -5,7 +5,8 @@ from typing import Optional
 
 client = None
 database = None
-users_col, access_log_col = None, None
+users_col = None
+access_log_col = None
 
 def connect_to_database():
     global client, database, users_col, access_log_col
@@ -20,20 +21,24 @@ def connect_to_database():
         print(f"Exception occured whilst connecting to MongoDB: {e}")
 
 def register_user(name:str):
+    global users_col
     user = users_col.insert_one({ "name": name, "card_id": None })
     return user
 
 def delete_user(_id:ObjectId):
+    global users_col
     result =users_col.delete_one({ "_id": _id })
     return result.acknowledged and result.deleted_count > 0
 
 def register_card_to_user(_id:ObjectId, card_id:str):
+    global users_col
     user = get_user(_id)
     user['card_id'] = card_id
     result = users_col.replace_one({ "_id": _id }, user)
     return result
 
 def register_entry(tag_id:str, _id:ObjectId):
+    global users_col
     access_log_col.insert_one({ "tag_id": tag_id, "user_id": _id, "time": datetime.now() })
     if _id is not None:
         user = get_user(_id)
@@ -41,6 +46,7 @@ def register_entry(tag_id:str, _id:ObjectId):
         users_col.replace_one({ "_id": _id }, user)
 
 def get_user(_id:ObjectId):
+    global users_col
     user = users_col.find_one({ "_id": _id })
     return user
 
@@ -48,6 +54,7 @@ def get_entries_count(_id:ObjectId):
     return access_log_col.count_documents({ "user_id": _id })
 
 def get_user_by_card(card_id:int):
+    global users_col
     print(f"get_user_by_card(card_id:{str(card_id)})")
     # if users_col is None: return False
     print(f"\tusers_col : {users_col}")
@@ -56,11 +63,13 @@ def get_user_by_card(card_id:int):
     return user
 
 def get_users_by_card(card_id:int):
+    global users_col
     if users_col is None: return False
     users = list(users_col.find({"card_id": str(card_id)}))
     return users
 
 def remove_all_links_to_card(card_id:int):
+    global users_col
     if users_col is None: return False
     users = get_users_by_card(card_id)
     for user in users:
